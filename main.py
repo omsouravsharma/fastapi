a40b960057e43e80015422ff592ead198694bbb4
omsouravsharma <souravsharma.sep22@gmail.com>
2022-01-27 23:41:28 +0530

    main updated

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# request matches the get method and "/"
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API- Sourav"}
+
+@app.get("/posts")
+
+def get_posts():
+    return {"data": "This is post data"}
